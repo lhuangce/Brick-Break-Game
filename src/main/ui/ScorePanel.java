abf095e4d9440e2f0e43269ec55634ccd31a3c2b
LH <lhuangce@students.cs.ubc.ca>
2023-08-06 18:10:18 -0700

    Updated documentation

--- src/main/ui/ScorePanel.java
+++ src/main/ui/ScorePanel.java
@@ -10,12 +10,10 @@ import java.awt.*;
  */
 public class ScorePanel extends JPanel {
     private static final String SCORE_TXT = "Score: ";
-//    private static final String MISSILES_TXT = "Missiles remaining: ";
     private static final int LBL_WIDTH = 100;
     private static final int LBL_HEIGHT = 30;
     private final BrickBreakGame game;
     private final JLabel scoreLabel;
-//    private JLabel missilesLbl;
 
     // Constructs a score panel
     // EFFECTS: sets the background colour and draws the initial labels;
@@ -27,20 +25,14 @@ public class ScorePanel extends JPanel {
         scoreLabel.setPreferredSize(new Dimension(LBL_WIDTH, LBL_HEIGHT));
         scoreLabel.setForeground(Color.WHITE);
         scoreLabel.setFont(new Font("Arial", Font.BOLD, 16));
-//        missilesLbl = new JLabel(MISSILES_TXT + SIGame.MAX_MISSILES);
-//        missilesLbl.setPreferredSize(new Dimension(LBL_WIDTH, LBL_HEIGHT));
         add(scoreLabel);
-//        add(Box.createHorizontalStrut(10));
-//        add(missilesLbl);
     }
 
     // Updates the score panel
     // MODIFIES: this
-    // EFFECTS:  updates number of invaders shot and number of missiles
-    //           remaining to reflect current state of game
+    // EFFECTS:  updates current score in game
     public void update() {
         scoreLabel.setText(SCORE_TXT + game.getScore());
-//        missilesLbl.setText(MISSILES_TXT + (SIGame.MAX_MISSILES - game.getNumMissiles()));
         repaint();
     }
 }
